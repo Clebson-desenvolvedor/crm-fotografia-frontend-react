7362514244406f4136b5fcbd6592bda91c7a64f8
clebson.araujo <clebson.araujo@l5.com.br>
2024-11-02 16:06:30 -0300

    Destaque no ícone da página selecionada e mudança no bg-color

--- src/components/Aside/index.js
+++ src/components/Aside/index.js
@@ -1,22 +1,46 @@
 import "./index.css";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faUsers, faHandshake, faChartLine, faGear, faSliders, faCamera } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
 
 function Aside() {
     return (
         <aside>
             <nav>
                 <div className="nav-logo">
-                    <img src="/assets/images/logo.png" alt="Imagem de perfil" />
+                    <NavLink to="/" className={({isActive}) => (isActive ? 'active' : '')}>
+                        <img src="/assets/images/logo.png" alt="Imagem de perfil" />
+                    </NavLink>
                 </div>
                 <ul>
-                    <li title="Home"><FontAwesomeIcon icon={faHome} /></li>
-                    <li title="Clientes"><FontAwesomeIcon icon={faUsers} /></li>
-                    <li title="Serviços"><FontAwesomeIcon icon={faCamera} /></li>
-                    <li title="Leads"><FontAwesomeIcon icon={faHandshake} /></li>
-                    <li title="Dashboard"><FontAwesomeIcon icon={faChartLine} /></li>
-                    <li title="Preferências"><FontAwesomeIcon icon={faSliders} /></li>
-                    <li title="Configurações"><FontAwesomeIcon icon={faGear} /></li>
+                    <NavLink to="/" className={({isActive}) => (isActive ? 'active' : '')}>
+                        <li title="Home"><FontAwesomeIcon icon={faHome} /></li>
+                    </NavLink>
+
+                    <NavLink to="/clients" className={({isActive}) => (isActive ? 'active' : '')}>
+                        <li title="Clientes"><FontAwesomeIcon icon={faUsers} /></li>
+                    </NavLink>
+
+                    <NavLink to="/services" className={({isActive}) => (isActive ? 'active' : '')}>
+                        <li title="Serviços"><FontAwesomeIcon icon={faCamera} /></li>
+                    </NavLink>
+
+                    <NavLink to="/leads" className={({isActive}) => (isActive ? 'active' : '')}>
+                        <li title="Leads"><FontAwesomeIcon icon={faHandshake} /></li>
+                    </NavLink>
+
+                    <NavLink to="/dashboard" className={({isActive}) => (isActive ? 'active' : '')}>
+                        <li title="Dashboard"><FontAwesomeIcon icon={faChartLine} /></li>
+                    </NavLink>
+
+                    <NavLink to="/preferences" className={({isActive}) => (isActive ? 'active' : '')}>
+                        <li title="Preferências"><FontAwesomeIcon icon={faSliders} /></li>
+                    </NavLink>
+
+                    <NavLink to="/configurations" className={({isActive}) => (isActive ? 'active' : '')}>
+                        <li title="Configurações"><FontAwesomeIcon icon={faGear} /></li>
+                    </NavLink>
                 </ul>
             </nav>
         </aside>
